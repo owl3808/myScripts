1c7362e3ce578340ca99dd70e90ed26510d1409c
Kevin_Lo <owl3808@gmail.com>
2016-08-17 14:50:45 +0800

    q!

---
+++ init_new_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#config here
+myName=""
+myEmail=""
+
+
+sudo apt-get install -y vim git filezilla
+
+# regular upgrade
+sudo crontab -l > mycron
+echo "1 0 * * * sudo /bin/sh -c \"/usr/bin/apt-get update && /usr/bin/apt-get upgrade -y\">> /tmp/upgrade.log 2>&1" >> mycron
+sudo crontab mycron
+rm mycron
+
+
+# setup git
+git config --global user.name $myName
+git config --global user.email $myEmail
